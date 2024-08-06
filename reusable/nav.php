9bb6566d6c57cd96b4a69849da0d85dee03b77a5
bhargavsuthar <sutharbhargav1997@gmail.com>
2024-08-06 10:03:53 -0400

    Updated - Icons for Brand and Login and admin nav bars

--- reusable/nav.php
+++ reusable/nav.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Yoga Studio Class</title>
+  <script src="https://kit.fontawesome.com/92343099fb.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="./styles/styles.css" type="text/css" rel="stylesheet">
 </head>
@@ -12,33 +13,40 @@
     session_start();
 ?>
 
+
 <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Yoga Studio Class</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <i class="fas fa-dove"></i> Yoga Studio Class
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item px-2">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="add.php">Add a Yoga Class</a>
+                </li>
+                <?php if (!empty($_SESSION['email'])): ?>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="./admin/">
+                            <i class="fas fa-user-shield"></i> Admin
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                <?php else: ?>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="./admin/">
+                            <i class="fas fa-sign-in-alt"></i> Login
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="add.php">Add a Yoga Class</a>
-                    </li>
-                    <?php if (!empty($_SESSION['email'])): ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./admin/">Admin</a>
-                        </li>
-                    <?php else: ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./admin/">Login</a>
-                        </li>
-                    <?php endif; ?>
-                </ul>
+                <?php endif; ?>
+            </ul>
         </div>
     </div>
 </nav>
